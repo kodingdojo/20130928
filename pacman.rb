3e5ea1132e0b6f3a3977c3b50ddfd29a7724cdc9
Trung Ngo <ndtrung4419@gmail.com>
2013-09-28 15:17:51 +0700

    chin

--- pacman.rb
+++ pacman.rb
@@ -10,6 +10,11 @@ class PacmanField
                 [".", ".", "."]]
     end
 
+    def tick
+        @map[1][1] = " "
+        @map[1][0] = ">"
+    end
+
     def startGame
         @map[1][1] = "V"
     end
@@ -41,5 +46,13 @@ describe PacmanField do
         @field.startGame
         @field.turnLeft
         @field.map[1][1].must_equal ">"
+    end
+
+    it "moves one step to the left" do
+        @field.startGame
+        @field.turnLeft
+        @field.tick
+        @field.map[1][0].must_equal ">"
+        @field.map[1][1].must_equal " "
     end
 end
