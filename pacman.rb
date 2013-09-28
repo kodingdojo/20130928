460096d7fd31ef5f73ebf20080c1c7e19c6afbeb
Trung Ngo <ndtrung4419@gmail.com>
2013-09-28 14:48:50 +0700

    chin

--- pacman.rb
+++ pacman.rb
@@ -2,9 +2,25 @@ require 'minitest/spec'
 require 'minitest/autorun'
 
 class PacmanField
+    attr_accessor :map
+
+    def initialize
+        @map = [[".", ".", "."],
+                [".", ".", "."],
+                [".", ".", "."]]
+    end
 end
 
 describe PacmanField do
-    it "" do
+    before do
+        @field = PacmanField.new
+    end
+
+    it "starts with a map full of dots" do
+        @field.map.each { |row|
+            row.each { |cell|
+                cell.must_equal "."
+            }
+        }
     end
 end
