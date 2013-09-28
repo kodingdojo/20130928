6bb9a0b9a2a362e5c2eac5806199be0431203a6c
Nguyễn Phan Hải <hainp2604@gmail.com>
2013-09-28 14:53:02 +0700

    hainp

--- code.rb
+++ code.rb
@@ -2,7 +2,21 @@ require "minitest/autorun"
 require "minitest/spec"
 
 class Pacman
+  def game_end? (board)
+    'End'
+  end
 end
 
-describe PacmanTest do
+describe Pacman do
+  before do
+    @game = Pacman.new
+  end
+
+  describe "#game_end?" do
+    it "return 'End'" do
+      board = ["v   ", "   ", "   "]
+      @game.game_end?(board).must_equal 'End'
+    end
+  end
+
 end
