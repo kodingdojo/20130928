f64ee755f146e846a9fe02804bfefc3afb73d46e
Hoàng Minh Thắng <p@banphim.net>
2013-09-28 15:11:00 +0700

    just commit

--- pacman.rb
+++ pacman.rb
@@ -3,18 +3,20 @@ require 'minitest/autorun'
 
 class PacMan
 		attr_accessor :board
+		
     def initialize initBoard
 			@board = initBoard
     end
 
 		def started?
-			if @board ==   "....." \
+			if @board ==  "....." \
 										"....." \
 										"....." \
 										"....." \
 										"....."
 				true
-
+			else
+				false
 			end
 		end
 end
@@ -29,6 +31,15 @@ describe PacMan do
 																"....." \
 																".....")
             pacman.started?().must_equal true
+        end
+
+        it "returns `false` when the board is not full of points" do
+            pacman = PacMan.new("....." \
+																". ..." \
+																"....." \
+																"....." \
+																".....")
+            pacman.started?().must_equal false
         end
     end
 
