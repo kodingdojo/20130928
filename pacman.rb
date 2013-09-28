b274adf888f1e5d77f5a579efac77b39e5482672
Hoàng Minh Thắng <p@banphim.net>
2013-09-28 14:55:31 +0700

    first commit

---
+++ pacman.rb
@@ -0,0 +1,27 @@
+require 'minitest/spec'
+require 'minitest/autorun'
+
+class PacMan
+    def initialize initBoard
+  
+    end
+
+		def started?
+			true
+		end
+end
+
+
+describe PacMan do
+    describe "checks game start" do
+        it "returns `true` when the board is full of points" do
+            pacman = PacMan.new("....." \
+																"....." \
+																"....." \
+																"....." \
+																".....")
+            pacman.started?().must_equal true
+        end
+    end
+
+end
